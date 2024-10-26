0d3e9a21e3a7fc0a0d02e9a4c671eaafa4042861
Matthew Okner <115849717+maokner@users.noreply.github.com>
2024-10-26 15:08:35 -0700

    Update moisture_sensor.py

--- moisture_sensor.py
+++ moisture_sensor.py
@@ -1,5 +1,21 @@
 import random
+import RPi.GPIO as GPIO
+import time
 
 def get_moisture_level():
 
     return random.randint(1,100)
+
+
+def read_moisture():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(18, GPIO.IN)  
+
+    moisture_level = GPIO.input(18)
+    
+    GPIO.cleanup()
+    
+    return moisture_level
+
+moisture_value = read_moisture()
+print(f"Current moisture level: {moisture_value}")
